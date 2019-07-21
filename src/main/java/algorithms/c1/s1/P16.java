1251ae89c2b2db84df188c0e1516b39d5fc028ce
james darin <jdchandler88@gmail.com>
2019-07-21 17:05:00 -0400

    problem 16

---
+++ src/main/java/algorithms/c1/s1/P16.java
@@ -0,0 +1,10 @@
+package algorithms.c1.s1;
+
+public class P16 {
+
+    public static String exR1(int n) {
+        if (n <= 0) return "";
+        return exR1(n-3) + n + exR1(n-2) + n;
+    }
+
+}
---
+++ src/test/java/algorithms/c1/s1/P16Test.java
@@ -0,0 +1,13 @@
+package algorithms.c1.s1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P16Test {
+
+    @Test
+    void exR1() {
+        assertEquals("311361142246", P16.exR1(6));
+    }
+}
